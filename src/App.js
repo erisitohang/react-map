3cb25cce5c27e196cae6349b3d88033d41ea5f22
eri <eri321@gmail.com>
2021-01-17 11:45:45 +0800

    Display map with react-map-gl

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import MapGL from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
+const TOKEN =
+  "pk.eyJ1IjoiZXJpc2NvZGUiLCJhIjoiY2trMDlxaDVrMGZkYTJvcnJyajFkanU1biJ9.Ge01K3NA4kGEO0JlPoF6_g";
 function App() {
+  const [viewport, setViewport] = useState({
+    width: "100%",
+    height: 900,
+    latitude: 51.5049375,
+    longitude: -0.0964509,
+    zoom: 14,
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: "100vh" }}>
+      <MapGL
+        {...viewport}
+        onViewportChange={setViewport}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxApiAccessToken={TOKEN}
+      ></MapGL>
     </div>
   );
 }
