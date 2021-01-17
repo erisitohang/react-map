626cd3bebc6849fef7007578b380c2a4dd01f3f6
eri <eri321@gmail.com>
2021-01-17 13:44:36 +0800

    Added Pins component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import { useState } from "react";
-import MapGL from "react-map-gl";
-import "mapbox-gl/dist/mapbox-gl.css";
+import { useState, useEffect } from 'react';
+import MapGL from 'react-map-gl';
+import Pins from './components/Pins';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
-const TOKEN =
-  "pk.eyJ1IjoiZXJpc2NvZGUiLCJhIjoiY2trMDlxaDVrMGZkYTJvcnJyajFkanU1biJ9.Ge01K3NA4kGEO0JlPoF6_g";
+const { REACT_APP_API_URI, REACT_APP_MAPGL_TOKEN } = process.env;
 function App() {
-  const [viewport, setViewport] = useState({
-    width: "100%",
+  const maxDrivers = 50;
+  const latitude = 51.5049375;
+  const longitude = -0.0964509;
+  const mapOptions = {
+    width: '100%',
     height: 900,
-    latitude: 51.5049375,
-    longitude: -0.0964509,
+    latitude,
+    longitude,
     zoom: 14,
-  });
+  };
+  const [viewport, setViewport] = useState(mapOptions);
+  const [data, setData] = useState({ pickup_eta: 0, drivers: [] });
+  const END_POINT = `${REACT_APP_API_URI}/drivers?latitude=${latitude}&longitude=${longitude}&count=${maxDrivers}`;
+
+  useEffect(() => {
+    fetch(END_POINT)
+      .then((resp) => resp.json())
+      .then((json) => setData(json));
+  }, [END_POINT]);
 
   return (
-    <div style={{ height: "100vh" }}>
+    <div style={{ height: '100vh' }}>
       <MapGL
         {...viewport}
         onViewportChange={setViewport}
         mapStyle="mapbox://styles/mapbox/streets-v9"
-        mapboxApiAccessToken={TOKEN}
-      ></MapGL>
+        mapboxApiAccessToken={REACT_APP_MAPGL_TOKEN}
+      >
+        <Pins data={data} />
+      </MapGL>
     </div>
   );
 }
